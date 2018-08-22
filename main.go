df918e1bc10201fe750e31f58623c6ce17265fca
Jiantao Shen <jiantaos@a31651jiantaos.us.corp>
2018-08-22 09:59:52 -0400

    first main.go

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+   "net/http"
+   "gopkg.in/mgo.v2"
+)
+
 
 func main() {
-   /* This is my first sample program. */
-   fmt.Println("Hello, World!")
+}
+
+type Adapter func(http.Handler) http.Handler
+func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
+   for _, adapter := range adapters {
+      h = adapter(h)
+   }
+   return h
+}
+
+func withDB(db *mgo.Session) Adapter {
+   // return the Adapter
+   return func(h http.Handler) http.Handler {
+      // the adapter (when called) should return a new handler
+      return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+         // copy the database session
+         dbsession := db.Copy()
+         defer dbsession.Close() // clean up
+         // save it in the mux context
+         context.Set(r, "database", dbsession)
+         // pass execution to the original handler
+         h.ServeHTTP(w, r)
+      })
+   }
 }
